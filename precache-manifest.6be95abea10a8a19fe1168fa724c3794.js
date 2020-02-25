b47de3aeff7c4a0b1f826a39b38def4e10d7fb55
Brice VIGNAL <vignal.brice@gmail.com>
2020-02-25 17:02:14 +0100

    Updates

---
+++ precache-manifest.6be95abea10a8a19fe1168fa724c3794.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9c0b7e5c4dd35dd426271b64f6c228",
+    "url": "/mon-app/index.html"
+  },
+  {
+    "revision": "60fa37c210e9cd0a891b",
+    "url": "/mon-app/static/css/main.c1a59f88.chunk.css"
+  },
+  {
+    "revision": "7391f6f3b7e28498a934",
+    "url": "/mon-app/static/js/2.4bbf5628.chunk.js"
+  },
+  {
+    "revision": "9d0c1864f9b5cb5a4a4929eace9cee67",
+    "url": "/mon-app/static/js/2.4bbf5628.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60fa37c210e9cd0a891b",
+    "url": "/mon-app/static/js/main.c5f6a10c.chunk.js"
+  },
+  {
+    "revision": "1952d55881cfab09e18a",
+    "url": "/mon-app/static/js/runtime-main.95716fb9.js"
+  },
+  {
+    "revision": "886a665556086868270d117fe35d17bf",
+    "url": "/mon-app/static/media/EARTH.886a6655.gif"
+  },
+  {
+    "revision": "4100a677ae52718b0be9e252db272c05",
+    "url": "/mon-app/static/media/facebook.4100a677.png"
+  },
+  {
+    "revision": "13ee934ba7fb6c615658a376d7fc53cb",
+    "url": "/mon-app/static/media/google.13ee934b.png"
+  },
+  {
+    "revision": "7ba725daacaf4be4af44f22da092452b",
+    "url": "/mon-app/static/media/loading.7ba725da.gif"
+  },
+  {
+    "revision": "be01e1d41ec8cd1094dcc578aa7ac45e",
+    "url": "/mon-app/static/media/movies.be01e1d4.png"
+  }
+]);
